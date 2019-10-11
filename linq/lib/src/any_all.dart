90b2c0ae6bb665fbc6ab440b3e473ed5ed97734e
Sahand <sahandevs@gmail.com>
2019-10-11 09:17:05 +0330

    add any_all placeholder functions

---
+++ linq/lib/src/any_all.dart
@@ -0,0 +1,19 @@
+TSource any<TSource>(
+  Iterable<TSource> source,
+) {
+  assert(false, "Not implemented");
+}
+
+TSource any2<TSource>(
+  Iterable<TSource> source,
+  bool Function(TSource a) predicate,
+) {
+  assert(false, "Not implemented");
+}
+
+TSource all<TSource>(
+  Iterable<TSource> source,
+  bool Function(TSource a) predicate,
+) {
+  assert(false, "Not implemented");
+}
